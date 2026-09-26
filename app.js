2cc02a8eaf3a8acfb196085c0b9896f2f1e56e89
Luiz Henrique <luizzaquino1214@gmail.com>
2026-09-26 19:14:36 -0300

    desafios fim

--- app.js
+++ app.js
@@ -57,7 +57,64 @@ function tamanhoArray(array){
 tamanhoArray(ordem);//TESTE
 
 //Crie um array e utilize a função includes para verificar se um elemento específico está presente
-function verificacaoArray(){
-    
+function verificacaoArray(array, numero){
+    //console.log(array.includes(numero)); já da como true
+    if(array.includes(numero)){
+        console.log(`o número ${numero} pertence ao array!`);
+    } else{
+        console.log(`o número ${numero} não pertence ao array!`);
+    }
+}
+verificacaoArray(ordem, 10);//TESTE
+
+//Crie um array de strings e utilize includes para verificar se uma determinada string está presente.
+let palavras = ['grape', 'orange', 'apple', 'melon'];
+let palavraP = 'grape';//TESTE
+console.log(`A palavra '${palavraP}' está no array?`, palavras.includes(palavraP));
+
+//Desenvolva uma função que receba um array de objetos representando estudantes de um curso e um objeto que representa um estudante procurado. 
+//Depois retorne se o objeto está presente no array.
+function verificarObjetosArray(array, objeto){
+    return array.some(item => JSON.stringify(item) === JSON.stringify(objeto));
 }
 
+const alunos = [
+    { id: 1, nome: 'Lucas', idade: 30 },
+    { id: 2, nome: 'Lucia', idade: 19 },
+    { id: 3, nome: 'Ana', idade: 25 },
+    { id: 4, nome: 'Leo', idade: 16 }
+];
+
+let alunoProcurado = {id: 2, nome: 'Lucia', idade: 19};
+
+let objetoEstaPresente = verificarObjetosArray(alunos, alunoProcurado);
+
+if(objetoEstaPresente){
+    console.log('O aluno está presente no array.');
+} else{
+    console.log('O aluno não está presente no array');
+}
+
+//Crie uma função que receba um array de números e retorne a soma dos elementos pares e o produto dos elementos ímpares.
+function calcularSomaProduto(array){
+    let somaDePares = 0;
+    let produtoDosImpares = 1;
+
+    for(let numeros of array){
+        if(numeros % 2 === 0){
+            somaDePares += numeros;
+        } else{
+            produtoDosImpares *= numeros;
+        }
+    }
+
+    return{
+        somaDePares,
+        produtoDosImpares
+    }
+}
+
+let numeros = [1, 2, 3, 4, 5];
+let resultado = calcularSomaProduto(numeros);
+console.log("Soma dos pares:", resultado.somaDePares);
+console.log("Soma dos ímpares:", resultado.produtoDosImpares);
